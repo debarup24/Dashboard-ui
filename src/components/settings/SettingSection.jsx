00275d6985661067e16df31e875e6195a1a96404
Debarup <87480898+debarup24@users.noreply.github.com>
2025-02-04 14:06:39 +0530

    Updating Components

---
+++ src/components/settings/SettingSection.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SettingSection = () => {
+  return <div>SettingSection</div>;
+};
+
+export default SettingSection;
--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Header from "../components/common/Header";
+import Profile from "../components/settings/Profile";
+import Notifications from "../components/settings/Notifications";
 
 const SettingsPage = () => {
   return (
     <div className="flex-1 overflow-auto relative z-10">
       <Header title="Settings" />
 
-      <main></main>
+      <main className="max-w-4xl mx-auto py-6 px-4 lg:px-8">
+        <Profile />
+        <Notifications />
+      </main>
     </div>
   );
 };
